d2dcc55621eb5885e73aaed0e91c33c5a7c140ab
June Zhao <bz.bijun@gmail.com>
2022-12-14 23:15:10 -0800

    update script

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(
 app.use(express.json());
 
 //Serving Frontend
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Routes
 app.get("/", (req, res) => {
