483823fc40405d5d1837fd19618135f2efbd34e3
June Zhao <bz.bijun@gmail.com>
2024-02-03 10:09:28 -0800

    update user route

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -11,7 +11,7 @@ const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const res = await fetch("http://localhost:5001/api/users/login", {
+    const res = await fetch("https://beige-gazelle-gear.cyclic.app/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
--- client/src/hooks/useRegister.js
+++ client/src/hooks/useRegister.js
@@ -11,7 +11,7 @@ const useRegister = () => {
     setIsLoading(true);
     setError(null);
 
-    const res = await fetch("http://localhost:5001/api/users/register", {
+    const res = await fetch("https://beige-gazelle-gear.cyclic.app/api/users/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, email, password }),
